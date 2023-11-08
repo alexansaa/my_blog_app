c62366ade53c6a011f6c33ed500c3f18ce935c23
alexansaa <alexan.saa@gmail.com>
2023-11-08 14:36:15 -0500

    optional changes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  def self.update_counter(post_id, likes_count)
-    my_post = Post.find(post_id)
-    if my_post.nil?
-      puts 'Post not found'
-      return
-    end
-    my_post.update(likes_counter: likes_count)
+
+  after_save :update_like_counter
+
+  def update_like_counter(likes_count)
+    post.update(likes_counter: likes_count)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,25 +3,13 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  def self.update_counter(user_id, post_count)
-    my_user = User.find(user_id)
-    if my_user.nil?
-      puts 'User not found'
-      return
-    end
-    my_user.update(posts_counter: post_count)
+  after_save :update_post_counter
+
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
   end
 
-  def self.most_recent_comments(post_id, limit = 5)
-    my_post = Post.find(post_id)
-    if my_post.nil?
-      puts 'Post not found'
-      return
-    end
-    my_comments = my_post.comments.order(created_at: :desc).limit(limit)
-    my_comments.each do |c|
-      puts "#{c.user} #{c.text}"
-    end
-    my_comments
+  def update_post_counter(post_count)
+    user.update(posts_counter: post_count)
   end
 end
