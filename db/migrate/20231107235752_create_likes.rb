fa5b8ba48e9daee16b6e2605e243a0cc87a1eddf
alexansaa <alexan.saa@gmail.com>
2023-11-07 20:51:42 -0500

    models association and tables field names edit

--- db/migrate/20231107235752_create_likes.rb
+++ db/migrate/20231107235752_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.1]
   def change
     create_table :likes do |t|
-      t.integer :user_id, foreign_key: true
-      t.integer :post_id, foreign_key: true
+      t.integer :user, foreign_key: true
+      t.integer :post, foreign_key: true
 
       t.timestamps
     end
