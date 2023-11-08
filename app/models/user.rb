598aeceb017ffb117bf6175134d1678dadc5992f
alexansaa <alexan.saa@gmail.com>
2023-11-08 18:28:23 -0500

    validation development

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
 
+  validates :name, presence: true
+  validates :posts_counter, numerically: { only_integer: true, greater_then_or_equal_to: 0 }
+
   def most_recent_post
     posts.order(created_at: :desc).limit(3)
   end  private
