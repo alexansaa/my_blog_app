f7a986a542d5176e37e1dd4dffc22ebdd82cd0e1
alexansaa <alexan.saa@gmail.com>
2023-11-07 20:14:00 -0500

    required changes

--- db/migrate/20231107235752_create_likes.rb
+++ db/migrate/20231107235752_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.1]
   def change
     create_table :likes do |t|
-      t.integer :user_id
-      t.integer :post_id
+      t.integer :user_id, foreign_key: true
+      t.integer :post_id, foreign_key: true
 
       t.timestamps
     end
