885d53ea56fb2d9ec3a10bee6af70bf702e9baf8
alexansaa <alexan.saa@gmail.com>
2023-11-22 17:17:11 -0500

    show user tests

--- spec/Integration/show_user_spec.rb
+++ spec/Integration/show_user_spec.rb
@@ -3,11 +3,34 @@ require 'rails_helper'
 RSpec.describe 'User Page', type: :system do
   before do
     @user = FactoryBot.create(:user)
+    @recent_posts = @user.most_recent_post
   end
 
   describe 'single user page' do
-    it 'show the see all post button' do
-      # visit 
+    it 'show the user info correctly' do
+      visit user_path(@user)
+      expect(page).to have_css("img[src*='#{@user.photo}']")
+      expect(page).to have_content(@user.name)
+      expect(page).to have_content("Number of posts: #{@user.posts_counter}")
+      expect(page).to have_content(@user.bio)
+      @recent_posts.each do |post|
+        expect(page).to have_content(post.text)
+      end
+      expect(page).to have_content('See all posts!')
+    end
+
+    it 'click on post redirect to show post page' do
+      visit user_path(@user)
+      @recent_posts.each do |post|
+        find("a[href='/users/#{user.id}/posts/#{post.id}]").click
+        expect(page).to have_current_path(user_post_path(@user, post))
+      end
+    end
+
+    it 'click on see all post redirects to users post index page' do
+      visit user_path(@user)
+      find("a[href='/users/#{@user.id}/posts']").click
+      expect(page).to have_current_path(user_posts_path(@user))
     end
   end
 end
