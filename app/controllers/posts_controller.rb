fd415d7f25725c1826058942b5243e0698d03298
alexansaa <alexan.saa@gmail.com>
2023-11-23 13:01:58 -0500

    first approach, rebase dev new commits

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,20 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
   end
 
   def show
+    @current_user = current_user
     @user = User.includes(:posts).find(params[:user_id])
     @post = Post.find(params[:id])
     @comments = @post.comments
   end
 
   def new
-    @user = ApplicationController.current_user
+    @user = current_user
     @post = Post.new
     respond_to do |format|
       format.html { render :new, locals: { post: @post, user: @user } }
@@ -19,7 +22,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @user = ApplicationController.current_user
+    @user = current_user
     @new_post = @user.posts.new(params.require(:post).permit(:title, :text))
     @new_post.comments_counter = 0
     @new_post.likes_counter = 0
