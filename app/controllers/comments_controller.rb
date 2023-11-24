b568950cff645e7531f50c20e530f13d7798025e
alexansaa <alexan.saa@gmail.com>
2023-11-24 16:44:27 -0500

    linter fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,7 +19,6 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @new_comment.save
         flash[:success] = 'Comment saved suvvessfully'
-        # format.html { redirect_to user_post_path(@user, @post), notice: 'Comment created' }
         format.js { render js: "window.location = '#{user_post_path(@user, @post)}';" }
       else
         flash.now[:error] = 'Error: Comment could not be saved'
@@ -37,14 +36,11 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @commnet.destroy
         flash[:success] = 'Comment deleted succesfully'
-        puts 'Comment deleted succesfully'
-        format.js { render js: "window.location = '#{user_post_path(user_id: @user.id, id: @post.id)}';" }
       else
         flash.now[:error] = 'Error: Comment could not be deleted'
         flash.now[:error_details] = @post.errors.full_messages.join(',')
-        puts 'Error: Comment could not be deleted'
-        format.js { render js: "window.location = '#{user_post_path(user_id: @user.id, id: @post.id)}';" }
       end
+      format.js { render js: "window.location = '#{user_post_path(user_id: @user.id, id: @post.id)}';" }
     end
   end
 end
