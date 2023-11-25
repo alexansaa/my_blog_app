3704596e86dc59c0dd646d352265e8a95fdb2136
unknown <macherki.nessrine@yahoo.com>
2023-11-25 02:53:15 +0100

    Updated users controller file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,31 @@
-class UsersController < ApplicationController
-  before_action :authenticate_user!
-
-  def index
-    @users = User.all
-    @user = current_user
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @posts = @user.most_recent_post
+module Api
+  class UsersController < Api::BaseController
+    before_action :authenticate_user!, except: [:create]
+  
+    def index
+      @users = User.all
+      @user = current_user
+    end
+  
+    def show
+      @user = User.find(params[:id])
+      @posts = @user.most_recent_post
+    end
+​
+    def create
+      @user = User.new(user_params)
+​
+      if @user.save
+        render json: @user, status: :created
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
+​
+    private
+​
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
   end
 end
